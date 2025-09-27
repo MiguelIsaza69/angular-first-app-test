cefa46bcf7eff781eb1bb48c24b5cfcc9f0f4d46
MiguelIsaza69 <jmm041721@gmail.com>
2025-09-27 17:29:09 -0500

    feat(lesson-14): Add HTTP communication

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,6 +5,7 @@ import { HousingLocation } from './housinglocation';
   providedIn: 'root'
 })
 export class HousingService {
+  url = 'http://localhost:3000/locations';
   protected housingLocationList: HousingLocation[] = [ {
       id: 0,
       name: 'Acme Fresh Start Housing',
@@ -38,15 +39,18 @@ export class HousingService {
 
   constructor() { }
 
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
 
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
   }
-
+  
   submitApplication(firstName: string, lastName: string, email: string) {
     console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
   }
 }
+
