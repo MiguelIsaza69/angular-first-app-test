95ac800bba749251c849be7877da850f0e52d40c
MiguelIsaza69 <jmm041721@gmail.com>
2025-09-27 17:49:10 -0500

    Plus: Improve application UI with CSS and solution errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { RouterOutlet, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true, // Esta línea es fundamental
   imports: [
     RouterOutlet,
-    HomeComponent
+    RouterModule,
+    
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
