b12237ec747311ec315c9eb49ecb8499c83b8fae
MiguelIsaza69 <jmm041721@gmail.com>
2025-09-27 13:24:28 -0500

    feat(lesson-04): Add housing location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html', // Corregimos la ruta
-  styleUrls: ['./home.component.css']
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent { // Aseguramos que el nombre de la clase sea este
-
+export class HomeComponent {
 }
+
